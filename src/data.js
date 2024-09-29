743159ac4d90b834250cb80924832fc5a5999041
suvojit Chatterjee <suvojitchatterjee84@gmail.com>
2024-09-29 08:52:33 +0530

    files added

---
+++ src/data.js
@@ -0,0 +1,224 @@
+const TshirtData = [
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e65345003afa8813b0/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e65415001328aabebe/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e65451002e5c59600d/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e654aa00282a5c797e/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e6550b00318a8ddb39/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e6553100102a6a5370/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e6557e0025451c87aa/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e65595002496ac4b8d/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e6564d001b5087af8d/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e6567b000eb9736a08/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e6569800021240ce78/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e656d000271a3abeed/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e656ec0013cac9e530/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e65785001503ceba15/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e65d98001f978dfba8/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e65d05002ea0ee46f8/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e65ce900388dbb05c6/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e65c630010a7a6dd02/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e65c0e00051bd9ff4a/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e65bea0028f2e8dc3d/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e65b8d0026d7acead2/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e65ae900242930b878/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e659930028df0713db/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e658b70020cd32ef43/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e657db003a3d45edb2/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66e657b500251f78a86f/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c3530006b977cd7f/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c4aa002f13c9c9af/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c4a10000cbb07f8a/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c49800315a4e3a32/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c48f0011018e4107/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c484002f77529ed7/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c4790014fb6f5bc4/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c46d001d132c985d/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c4610033b9b4365f/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c458000e9351e907/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c44c0028967fd301/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c41e00038919c9a0/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c411000a64ff2e47/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c40500361ca1c919/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c3d4002040b677eb/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c3c60012fcad700c/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c3b40008f4a44863/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c382000435a81893/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c376002bd8c1a997/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c36c000dc5d01ad6/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image:"https://cloud.appwrite.io/v1/storage/buckets/66e595b00013804120c2/files/66f8c3600026677a120d/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    }
+
+]
+const MousePadData = 
+[
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66dbf203000ba54424c8/files/66e5927f002927078954/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66dbf203000ba54424c8/files/66e592c000275879812e/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66dbf203000ba54424c8/files/66e59302001ecb1b7bec/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66dbf203000ba54424c8/files/66e5931d0026bc5ecb49/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66dbf203000ba54424c8/files/66e59343000307541e2f/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66dbf203000ba54424c8/files/66e59350003ab6baf507/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66dbf203000ba54424c8/files/66e5944e00334959b5c7/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66dbf203000ba54424c8/files/66e594af00192460964d/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66dbf203000ba54424c8/files/66e594be002f759d3b3d/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66dbf203000ba54424c8/files/66e594d7003ab62b6f2e/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66dbf203000ba54424c8/files/66e594ec00195f369c5f/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66dbf203000ba54424c8/files/66e59505003057d1b78c/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    }
+]
+
+const MugData = [
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e59534001f22e57315/files/66e59597001c1d494b45/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },  
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e59534001f22e57315/files/66e599a4000f269aad05/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e59534001f22e57315/files/66e599bc001a2edf9d9d/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },  
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e59534001f22e57315/files/66e599d70038dbe72e2b/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e59534001f22e57315/files/66e599e6001edaac9e27/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },  
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e59534001f22e57315/files/66e59a0600073b98cb1a/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e59534001f22e57315/files/66e59a360018faca8369/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },  
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e59534001f22e57315/files/66e59a570005c6d67871/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    }
+]
+
+const MetalSheetData = [
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e59534001f22e57315/files/66e59c7a00150fac1b66/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e59534001f22e57315/files/66e59c89002a19e7ecec/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    },
+    {
+        image: "https://cloud.appwrite.io/v1/storage/buckets/66e59534001f22e57315/files/66e59c940030ce06f1ed/view?project=66dbdbc5003389a57441&project=66dbdbc5003389a57441&mode=admin"
+    }
+]
+
+export { TshirtData ,MousePadData ,MugData,MetalSheetData};
